d91f128aaaf92deae0b15b752812206738e41e82
Pascal <buehler1991@gmail.com>
2023-11-05 16:56:08 +0100

    added histogram thread

--- rasperry_run/state_and_dice_multithread.py
+++ rasperry_run/state_and_dice_multithread.py
@@ -1,7 +1,32 @@
-# main.py
-
 import cv2
+import csv
 import utils.threading_utils as tu
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from threading import Thread
+import time
+import os
+
+
+
+# New imports and functions
+def update_histogram():
+    plt.ion()  # Enable interactive mode
+    fig, ax = plt.subplots()
+    while True:  # This loop will keep checking for changes and update the histogram
+        if os.path.exists('/results/res.csv'):
+            df = pd.read_csv('/results/res.csv')
+            ax.clear()
+            df.hist(ax=ax)  # Replace with the column name if your CSV has headers
+            plt.draw()
+            plt.pause(0.01)  # Pause to ensure the plot updates
+        time.sleep(1)  # Check for updates every second or more
+
+
+# Start histogram thread
+histogram_thread = Thread(target=update_histogram)
+histogram_thread.start()
 
 # Initialize webcam
 cap = cv2.VideoCapture(0)
@@ -47,13 +72,16 @@ while True:
     # Show the frame
     cv2.imshow('frame', frame)
 
-    # Break the loop if 'q' is pressed
-    if cv2.waitKey(1) & 0xFF == ord(' '):
-        print("hey")
 
+    # save result to file 
+    if cv2.waitKey(1) & 0xFF == ord(' '):
+            with open('/results/res.csv', mode='a') as file:
+                writer = csv.writer(file)
+                writer.writerow([dice_sum])
 
     # Break the loop if 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        histogram_thread.join()
         break
 
 # Stop the worker threads and release resources
