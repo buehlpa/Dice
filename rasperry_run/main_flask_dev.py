950cee283a0a0d05855ae8bd2c15f591de7d8740
bule <bule@zhaw.ch>
2023-11-18 11:23:46 +0100

    debug = true

--- rasperry_run/main_flask_dev.py
+++ rasperry_run/main_flask_dev.py
@@ -281,4 +281,4 @@ def open_browser():
 
 if __name__ == '__main__':
     threading.Timer(0.5, open_browser).start()
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000,debug=True)
